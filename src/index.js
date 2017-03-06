2fc7a42f222fce5a96d83c6979753ec53847c9a3
etiennedilocker <etienne@dilocker.de>
2017-03-06 22:20:12 +0100

    trying to parse json

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 // @flow
 
-const foo: string = 'asdf';
-console.log('hello', foo);
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const foo = readFileSync(join(__dirname, 'coverage.json'));
+
+const fooParsed = JSON.parse(foo)['src/FilterGroup.js'];
+
+console.log(JSON.stringify(fooParsed, null, 2));
